0931f778329f90376168fe882bf1068164add8eb
Timothée Floure <timothee.floure@fnux.ch>
2017-03-02 10:12:00 +0100

    Add an admin role for user management

--- app/controllers/inventory_controller.rb
+++ app/controllers/inventory_controller.rb
@@ -1,5 +1,5 @@
 class InventoryController < ApplicationController
-  before_action :authenticate_user!
+before_action :authenticate_user!
 
   def index
     @q = Line.ransack(params[:q])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :authenticate_admin!
 
   def index
     @users = User.all
@@ -39,6 +40,14 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def authenticate_admin!
+    if current_user.try(:admin?)
+      true
+    else
+      redirect_to root_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
