68ff629614b73f635a8e8410361ad21329b34dc9
Timothée Floure <timothee.floure@fnux.ch>
2017-03-02 23:01:27 +0100

    Set application title in config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Inventory
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.application_title = "PolyJapan EPFL"
   end
 end
